bee79a9e02c23c43e1a823c8ae724a8c8aeb4727
Lingen4 <lucrfb@gmail.com>
2024-12-16 13:39:27 +0100

    Nivel 3 completo

--- NIVEL3/Cine.php
+++ NIVEL3/Cine.php
@@ -1,31 +1,68 @@
 <?php
 
-class Cine {
+class Cine
+{
 
     private $nombre;
     private $poblacion;
     private $listado;
 
 
-    public function __construct(string $nombre, string $poblacion, array $listado=[])
+    public function __construct(string $nombre, string $poblacion, array $listado = [])
     {
-        $this-> nombre = $nombre;
-        $this-> poblacion = $poblacion;
-        $this-> listado =$listado;
+        $this->nombre = $nombre;
+        $this->poblacion = $poblacion;
+        $this->listado = $listado;
     }
 
     public function setListado(array $listado): void
     {
+
         $this->listado = $listado;
     }
-
-    public function agregarPelicula(Pelicula $pelicula): void
+    public function getListado(): array
     {
-        $this->listado[] = $pelicula;
+        return $this->listado;
     }
 
+    public function getNombre(): string
+    {
+        return $this->nombre . " ($this->poblacion)";
+    }
+
+    public function getPeliculas()
+    {
+
+        return array_map(fn($pelicula) => (string)$pelicula, $this->listado);
+    }
+
+    public function getCinePorDirector(string $nombreDirector)
+    {
+
+        $peliculas = $this->getListado();
+        $peliculasDirector = [];
+        foreach ($peliculas as $pelicula) {
+            if (strtolower($pelicula->getDirector()) == strtolower($nombreDirector)) {
+                $peliculasDirector = [$pelicula];
+            }
+        }
+
+        return $peliculasDirector;
+    }
+
+    public function peliLarga(): object
+    {
+
+        $peliculas = $this->listado;
+        $duracion = 0;
+        $peliculaLarga = null;
+        foreach ($peliculas as $pelicula) {
+
+            if ($duracion < ($pelicula->getTime())) {
+                $duracion = $pelicula->getTime();
+                $peliculaLarga = $pelicula;
+            }
+        }
+        return $peliculaLarga;
+    }
 }
-
-
-
-?>
--- NIVEL3/Pelicula.php
+++ NIVEL3/Pelicula.php
@@ -7,11 +7,36 @@ class Pelicula {
     private $duracion;
 
 
-    public function __construct(string $nombre, string $director, int|float $duracion):void
+    public function __construct(string $nombre, string $director, int|float $duracion)
     {
         $this->nombre=$nombre;
         $this->director=$director;
         $this->duracion=$duracion;
+   
     }
-        
+    public function __toString()
+    {
+        $datos =  "\n\nPelicula: ".$this->nombre;
+         $datos.= "\nDirector: ".$this->director;
+         $datos.= "\nDuración: ".$this->duracion." minutos";
+
+         return $datos;
+    }
+
+    public function getTitulo(){
+
+        return $this->nombre;
+    }
+
+    public function getTime(){
+
+        return $this->duracion;
+    }
+
+    public function getDirector(){
+
+        return $this->director;
+    }
+
+
     }
